b6d462043c050696d709acda06ce3f8a00a9a547
Darren Sayo <60412474+dvrsayo@users.noreply.github.com>
2025-09-02 04:21:13 +0000

    task3 java

--- Task3.java
+++ Task3.java
@@ -3,12 +3,12 @@ public class Task3 {
     public static void main(String[] args) {
     String a = new String("Wow");
     String b = a;
-    String c = "Woww";
+    String c = "wow";
     String d = "Wow!";
 
     boolean b1 = a == b; //true (same object reference)
     boolean b2 = d.equals(b + "!"); //true since both equal "Wow!"
-    boolean b3 = !c.equals(a); //true since "Woww" does not equal "Wow"
+    boolean b3 = !c.equals(a); //true since "wow" does not equal "Wow"
 
     if (b1 && b2 && b3) {
     System.out.println("Success!");
