a83e31a64cae57a1162b66f1350a0fd7e4bd7497
Darren Sayo <60412474+dvrsayo@users.noreply.github.com>
2025-09-08 01:58:43 +0000

    task5

---
+++ Task5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task5{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first Number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+        
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } 
+        else if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        } 
+        else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        } 
+        else {
+            System.out.println("The largest number is: " + num3);
+        }
+        
+        scanner.close();
+    }
+}
